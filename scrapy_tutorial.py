47aa4f1e3b16bad96b5afb3d6be465fbe0f48f00
KawanaS <37668110+KawanaS@users.noreply.github.com>
2022-08-02 05:59:21 +0800

    Update scrapy_tutorial.py

--- scrapy_tutorial.py
+++ scrapy_tutorial.py
@@ -1,30 +1,30 @@
 import bs4
+import requests
 import urllib.request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-
-url='https://www.newegg.com/Drones/SubCategory/ID-3708?cm_sp=Cat_Drones_1-_-VisNav-_-Drones//c1.neweggimages.com/WebResource/Themes/2005/Nest/logo_424x210.png'
+url='https://www.ebay.com/sch/i.html?_from=R40&_nkw=%E4%B8%83%E5%BD%A9%E8%99%B9+graphics+card&_sacat=0&LH_TitleDesc=0&rt=nc&Brand=Colorful&_dcat=27386'
 page=urllib.request.urlopen(url)
 page_soup=BeautifulSoup(page,'html.parser')
-img_items=page_soup.find('div',{'class':'items-view is-grid'})
-img_div=img_items.find_all(class_='item-container')
+img_items=page_soup.find('div',id='srp-river-results')
+img_div=img_items.find_all('div',{'class':'s-item__image'})
 
 i=1
 for img in img_div:
-    img_tag=img.find('img')
+    img_tag=img.find('img',class_='s-item__image-img')
     img_src=img_tag.get('src')
-    if img_src[:1]=='/':
-        image='https:'+img_src
-    else:
-        image=img_src
+    #img_title=img_tag.get('title')
+    #if img_src[:1]=='/':
+        #image='https:'+img_src
+    #else:
+    image=img_src
     print(image)
     file_name=str(i)
     i+=1
     img_file=open(file_name +'.jpeg','wb')
     img_file.write(urllib.request.urlopen(image).read())
     img_file.close()
-
 
 
 
